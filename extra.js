8bd98339d243c14ebb8b351154decddb75136b4e
destroyed12121 <definitelyliam0@gmail.com>
2026-01-07 19:23:54 -0500

    aa

--- extra.js
+++ extra.js
@@ -1,4 +1,36 @@
- Finish the extra page
-     Add dummy text
-         Check styling
-             Test link in footer
+TODO
+fix the proxy
+fix panic panickey, add quote talking about it
+fix theatermode, make it hide buttons after a few seconds and ensure all content is being displayed and move the description to the bottom
+change back player to use old styling, change the surface to use background colors for the bottom part where description is shown, looking just like the background
+add image and  video support for backgrounds
+fix aichat, make the title use the same ai request as the users question unless its in image mode where it will use text mode and fix ai chat to remember modes better
+add cloaking to quotes
+add a default cloak and fix it so icons work in aboutblank for whatever reason they only work when you go to settings in aboutblank where it correctly applies
+for search suggestions add highlighting for whatever will happen when you click ElementInternals, in search suggestions fix all games not sending you to the games page
+
+
+
+add singlefile using cdns for each page so it autoupdates and is easy enough to make
+
+prolly should advertise phantom
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
