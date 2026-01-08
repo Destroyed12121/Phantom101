452425b6349da074c3d81934ceb72239c9aeb8d4
destroyed12121 <definitelyliam0@gmail.com>
2026-01-07 20:00:48 -0500

    a

--- extra.js
+++ extra.js
@@ -1,37 +1,20 @@
 TODO
-fix the proxy
-fix panic panickey, add quote talking about it
-fix theatermode, make it hide buttons after a few seconds and ensure all content is being displayed and move the description to the bottom
-change back player to use old styling, change the surface to use background colors for the bottom part where description is shown, looking just like the background
-add image and  video support for backgrounds
-fix aichat, make the title use the same ai request as the users question unless its in image mode where it will use text mode and fix ai chat to remember modes better
-add cloaking to quotes
-add a default cloak and fix it so icons work in aboutblank for whatever reason they only work when you go to settings in aboutblank where it correctly applies
-for search suggestions add highlighting for whatever will happen when you click ElementInternals, in search suggestions fix all games not sending you to the games page
-make topbar open 
-fix games to make multi library the default, put it in config.js
-fix music page to hide youtube player unless toggled via 3 dot small button, and always shown on proxy mode, fix all the controls for video time, volume,
-add singlefile using cdns for each page so it autoupdates and is easy enough to make
-prolly should advertise phantom
-revamp movies page to watch (lowk copy vapor v4) and add youtube and twitch
-shorten some page's code
-1/8/2026
+    - fix the proxy
+        - fix panic panickey, add quote talking about it
+            - fix theatermode, make it hide buttons after a few seconds and ensure all content is being displayed and move the description to the bottom
+                - change back player to use old styling, change the surface to use background colors for the bottom part where description is shown, looking just like the background
+                    - add image and video support for backgrounds
+                        - fix aichat, make the title use the same ai request as the users question unless its in image mode where it will use text mode and fix ai chat to remember modes better
+                            - add cloaking to quotes
+                                - add a default cloak and fix it so icons work in aboutblank for whatever reason they only work when you go to settings in aboutblank where it correctly applies
+                                    -for search suggestions add highlighting for whatever will happen when you click ElementInternals, in search suggestions fix all games not sending you to the games page
+                                        - fix games to make multi library the default, put it in config.js
+                                            - fix music page to hide youtube player unless toggled via 3 dot small button, and always shown on proxy mode, fix all the controls for video time, volume,
+                                                -add singlefile using cdns for each page so it autoupdates and is easy enough to make
+                                                    - prolly should advertise phantom
+                                                        - revamp movies page to watch(lowk copy vapor v4) and add youtube and twitch
+                                                            - shorten some page's code
+                                                                - 1.0.8! Release - Finalizing all features and fixes for the upcoming rollout
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+latest version 1.0.8 dev
+1 / 8 / 2026
